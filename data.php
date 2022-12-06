0d548b1095e9cc2c3a3c6724b0918a3a553dee89
361786 <113468648+jomeis@users.noreply.github.com>
2022-12-06 01:07:16 +0000

    i made the scatter plot on the data.php

--- data.php
+++ data.php
@@ -1,27 +1,17 @@
+
+
+
+
+
+
+
+
+<!-- Graph and template sepp -->
+
+
+
 <?php include $_SERVER['DOCUMENT_ROOT'].'/functions/functions.php'; ?>
-<?php
- 
-$dataPoints = array(
-	array("x"=> 5, "y"=> 5),
-	array("x"=> 7, "y"=> 6),
-	array("x"=> 2, "y"=> 7),
-	array("x"=> 8, "y"=> 6.5),
-	array("x"=> 6, "y"=> 9.5),
-	array("x"=> 2.5, "y"=> 8),
-	array("x"=> 9, "y"=> 6),
-	array("x"=> 8.6, "y"=> 6),
-	array("x"=> 9, "y"=> 5),
-	array("x"=> 6, "y"=> 6),
-	array("x"=> 1, "y"=> 3),
-	array("x"=> 10, "y"=> 8),
-	array("x"=> 6, "y"=> 7),
-	array("x"=> 3, "y"=> 1),
-	array("x"=> 4, "y"=> 9.9),
-	array("x"=> 2, "y"=> 9.6),
-	array("x"=> 1, "y"=> 9.8)
-);
-	
-?>
+
 
 <!doctype html>
 <html lang="en">
@@ -57,43 +47,33 @@ $dataPoints = array(
       </div> <!--END: container  -->
 
       <div class="row"> <!--START: top row  -->
-        <div class="col p-2 d-flex justify-content-center" id="content"> <!-- START: col-->
-          <h2>Chatacters and their teirs</h2>
-        </div> <!-- END: top row  -->
-      </div> <!--END: container  -->
-      <div class="row"> <!--START: top row  -->
-        <div class="col p-2 d-flex justify-content-center" id="content"> <!-- START: col-->
-          <script>
-window.onload = function () {
- 
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	exportEnabled: true,
-	theme: "light1", 
-	title:{
-		text: "Character Rank Vs Difficulty to Learn"
-	},
-	axisX:{
-		title: "Difficulty",
-	},
-	axisY:{
-		title: "Rank",
-		suffix: " Teir"
-	},
-	data: [{
-		type: "scatter",
-		markerType: "square",
-		markerSize: 10,
-		toolTipContent: "Rank: {y} inch<br>Difficulty: {x} kg<br>Name:",
-		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-	}]
-});
-chart.render();
- 
-}
+        <div class="col p-2" id="content"> <!-- START: col-->
+          <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+<body>
+
+<div id="myPlot" style="width:100%;max-width:700px"></div>
+
+<script>
+var xArray = [50,60,70,80,90,100,110,120,130,140,150];
+var yArray = [7,8,8,9,9,9,10,11,14,14,15];
+
+// Define Data
+var data = [{
+  x:xArray,
+  y:yArray,
+  mode:"markers"
+}];
+
+// Define Layout
+var layout = {
+  xaxis: {range: [40, 160], title: "Square Meters"},
+  yaxis: {range: [5, 16], title: "Price in Millions"},  
+  title: "House Prices vs. Size"
+};
+
+// Display using Plotly
+Plotly.newPlot("myPlot", data, layout);
 </script>
-
-
         </div> <!-- END: top row  -->
       </div> <!--END: container  -->
 
@@ -109,8 +89,14 @@ chart.render();
 
     </div> <!--END: col  -->
 
-<div id="chartContainer" style="height: 170px; width: 50%;"></div>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
+
+
+
+
+
+
+
--- test.php
+++ test.php
@@ -1,28 +1,19 @@
+
+
+
+
+
+
+
+
+<!-- Graph and template sepp -->
+
+
+
 <?php include $_SERVER['DOCUMENT_ROOT'].'/functions/functions.php'; ?>
-<?php
- 
-$dataPoints = array(
-	array("x"=> 51, "y"=> 51),
-	array("x"=> 77, "y"=> 56),
-	array("x"=> 82, "y"=> 57),
-	array("x"=> 82.5, "y"=> 63.5),
-	array("x"=> 83, "y"=> 59.5),
-	array("x"=> 85, "y"=> 58),
-	array("x"=> 90, "y"=> 65),
-	array("x"=> 97.5, "y"=> 66),
-	array("x"=> 99, "y"=> 59),
-	array("x"=> 102, "y"=> 64),
-	array("x"=> 102, "y"=> 63),
-	array("x"=> 111.5, "y"=> 68),
-	array("x"=> 112, "y"=> 70),
-	array("x"=> 113, "y"=> 61),
-	array("x"=> 128, "y"=> 65),
-	array("x"=> 132, "y"=> 66.5),
-	array("x"=> 151, "y"=> 71)
-);
-	
-?>
-<!DOCTYPE HTML>
+
+
+<!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -32,7 +23,9 @@ $dataPoints = array(
 
     <link href="/style.css" rel="stylesheet" >
   </head>
-   <div class="container">
+  <body>
+
+    <div class="container">
       <div class="row">
         <div class="col nav fixed-top">
           <?php
@@ -41,6 +34,8 @@ $dataPoints = array(
         </div>
       </div>
     </div>
+    
+    
     <div class="container"> <!--START: container  -->
 
 
@@ -50,48 +45,58 @@ $dataPoints = array(
             <a href="https://www.guiltygear.com/ggst/en/" target="_blank"><img src="/images/guiltyGearTitle2.png"></a>
         </div> <!-- END: top row  -->
       </div> <!--END: container  -->
-<head>  
-This is above
-<script>
-window.onload = function () {
- 
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	exportEnabled: true,
-	theme: "light1", 
-	title:{
-		text: "Study of Height vs Weight"
-	},
-	axisX:{
-		title: "Weight",
-		suffix: " kg"
-	},
-	axisY:{
-		title: "Height",
-		suffix: " inch"
-	},
-	data: [{
-		type: "scatter",
-		markerType: "square",
-		markerSize: 10,
-		toolTipContent: "Height: {y} inch<br>Weight: {x} kg",
-		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-	}]
-});
-chart.render();
- 
-}
-</script>
-</head>
+
+      <div class="row"> <!--START: top row  -->
+        <div class="col p-2" id="content"> <!-- START: col-->
+          <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 <body>
-<div id="chartContainer" style="height: 170px; width: 50%;"></div>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-   <div class="row"> <!--START: top row  -->
+
+<div id="myPlot" style="width:100%;max-width:700px"></div>
+
+<script>
+var xArray = [50,60,70,80,90,100,110,120,130,140,150];
+var yArray = [7,8,8,9,9,9,10,11,14,14,15];
+
+// Define Data
+var data = [{
+  x:xArray,
+  y:yArray,
+  mode:"markers"
+}];
+
+// Define Layout
+var layout = {
+  xaxis: {range: [40, 160], title: "Square Meters"},
+  yaxis: {range: [5, 16], title: "Price in Millions"},  
+  title: "House Prices vs. Size"
+};
+
+// Display using Plotly
+Plotly.newPlot("myPlot", data, layout);
+</script>
+        </div> <!-- END: top row  -->
+      </div> <!--END: container  -->
+
+      <div class="row"> <!--START: top row  -->
         <div class="col p-2" id="foot"> <!-- START: col-->
  
           footer
          
         </div> <!-- END: top row  -->
       </div> <!--END: container  -->
-</body>
-</html>             
+
+
+
+    </div> <!--END: col  -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
+
+
+
+
